056f17f9eb100380aebd9c76074ecedfac6f5518
Rufus Pollock <rufus.pollock@okfn.org>
2017-06-24 11:50:57 +0200

    [showcase][m]: wire in new lib stuff to load data from bitstore.
    Showcase page now working with live data!

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 'use strict'
 
 var express = require('express')
+var request = require('request')
 
+var config = require('../config')
+var lib = require('../lib')
 
 module.exports = function() {
   // eslint-disable-next-line new-cap
@@ -12,8 +15,16 @@ module.exports = function() {
     });
 	})
 
-  router.get('/:owner/:id', function (req, res) {
+  router.get('/:owner/:name', async function (req, res) {
+    const api = new lib.DataHubApi(config)
+    const dpjson = await api.getPackage(req.params.owner, req.params.name)
+    const readme = await api.getPackageFile(req.params.owner, req.params.name, 'README.md')
+    const dpBitStoreUrl = [config.get('bitstoreBaseUrl'), 'metadata', req.params.owner, req.params.name, '_v', 'latest'].join('/')
     res.render('showcase.html', {
+       dataset: dpjson,
+       datapackageUrl: dpBitStoreUrl,
+       readmeShort: '',
+       readme_long: readme
     });
 	})
 
