df89c4716653721d01bc98a09e5a345eba699080
anuveyatsu <anuar.ustayev@gmail.com>
2017-07-31 10:49:30 +0600

    [error-handler][xs]: error handler that displays simple msg for user and prints details in logs - refs #44

--- index.js
+++ index.js
@@ -40,6 +40,11 @@ module.exports.makeApp = function () {
     routes()
   ])
 
+  app.use((err, req, res) => {
+    console.error(err.stack)
+    res.status(500).send('Something failed. Please, try again later.')
+  })
+
 	// eslint-disable-next-line no-unused-vars
   const env = nunjucks.configure(app.get('views'), {
     autoescape: true,
