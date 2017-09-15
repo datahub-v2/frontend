0ab621866820215d066d709cad4649ae0ee860ea
anuveyatsu <anuar.ustayev@gmail.com>
2017-09-15 15:20:50 +0600

    [pipelines][s]: handle INVALID state of pipelines - refs #96

--- routes/index.js
+++ routes/index.js
@@ -198,7 +198,8 @@ module.exports = function () {
     const userAndPkgId = await api.resolve(path.join(req.params.owner, req.params.name))
     try {
       const status = await api.pipelineStatus(userAndPkgId.userid, userAndPkgId.packageid, 'info')
-      if (status.state === 'FAILED' || status.state === 'SUCCEEDED') {
+      const resolvedStates = ['SUCCEEDED', 'FAILED', 'INVALID']
+      if (resolvedStates.includes(status.state)) {
         res.render('pipelines.html', {
           status
         })
